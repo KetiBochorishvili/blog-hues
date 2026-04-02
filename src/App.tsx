cfa403824fd381a6a4ab3e9743ff7c20e52cf61f
KetiBochorishvili <ketevani.boch@gmail.com>
2026-04-02 16:14:20 +0400

    fix: browserrouter

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/blog-hues">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
